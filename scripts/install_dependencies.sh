d78c58941cab228f62899d849bca639016e06b96
edul77 <140404799+edul77@users.noreply.github.com>
2025-05-10 10:51:44 +0000

    Update install_dependencies.sh
    edited the install_dependencies.sh file

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-cd /home/ec2-user/angular-app
-echo "Installing Angular app dependencies..."
-npm install
-ng build --configuration production
+sudo apt update
+sudo apt install apache2 -y
